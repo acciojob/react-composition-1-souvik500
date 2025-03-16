b1b8f4f76758589a6f704313e59d52724aa6ba5e
souvik500 <120314952+souvik500@users.noreply.github.com>
2025-03-17 01:10:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [tags, setTags] = useState("");
+
+  const handleTags = (e) => {
+    setTags(e);
+  };
+  
   return (
     <div>
         {/* Do not remove the main div */}
+      <h2 onClick={() => handleTags("qa")}>*Tag 1</h2>
+        <h2 onClick={() => handleTags("wa")}>*Tag 2</h2>
+        <h2 onClick={() => handleTags("ed")}>*Tag 3</h2>
+
+        <p>Choosen Tag is: {tags} </p>
     </div>
   )
 }
